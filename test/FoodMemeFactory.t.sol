6f7b520052a97795f37627be9deca86d322d0aa6
polymorpher <aaron@hiddenstate.xyz>
2024-10-19 22:21:59 -0700

    dummy test

---
+++ test/FoodMemeFactory.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: CC-BY-NC-4.0
+pragma solidity ^0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+import {FoodMemeFactory} from "contracts/FoodMemeFactory.sol";
+
+contract FoodMemeFactoryTest is Test {
+    //    FoodMemeFactory public factory;
+
+    function setUp() public {}
+
+    function test_Increment() public {}
+}
