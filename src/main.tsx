38a81bf6795a5056f11c4379591744b701499ed4
polymorpher <aaron@hiddenstate.xyz>
2024-10-20 07:34:44 -0700

    rainbow wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
+import '@rainbow-me/rainbowkit/styles.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import Launch from './launch'
 import Mint from './mint'
+
+import {
+  getDefaultConfig,
+  RainbowKitProvider
+} from '@rainbow-me/rainbowkit'
+import { WagmiProvider } from 'wagmi'
+import {
+  polygon,
+  base,
+  harmonyOne
+} from 'wagmi/chains'
+import {
+  QueryClientProvider,
+  QueryClient
+} from '@tanstack/react-query'
 
 const App = (): React.JSX.Element => {
   return (
