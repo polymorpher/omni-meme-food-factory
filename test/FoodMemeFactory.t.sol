196dd33b77fdaab5c7fee7b1c76ec64034914785
polymorpher <aaron@hiddenstate.xyz>
2024-10-20 07:42:30 -0700

    fix ci

--- test/FoodMemeFactory.t.sol
+++ test/FoodMemeFactory.t.sol
@@ -20,8 +20,8 @@ import {OFTComposeMsgCodec} from "@layerzerolabs/oft-evm/libs/OFTComposeMsgCodec
 import {TestHelperOz5} from "./helper/TestHelperOz5.sol";
 
 contract FoodMemeFactoryTest is TestHelperOz5 {
-    uint256 DEPLOYER_PRIVATE_KEY =
-        vm.envOr("DEPLOYER_PRIVATE_KEY", "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
+    uint256 defaultTestKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+    uint256 DEPLOYER_PRIVATE_KEY = vm.envOr("DEPLOYER_PRIVATE_KEY", defaultTestKey);
     address deployer = vm.addr(DEPLOYER_PRIVATE_KEY);
 
     using OptionsBuilder for bytes;
